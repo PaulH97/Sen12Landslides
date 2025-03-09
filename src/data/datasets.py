61d06ee50cb9283e3be82dfef4bb8454801d6010
Paul Höhn <paul.hoehn@dlr.de>
2025-03-09 11:40:30 +0100

    Remove h5

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -4,29 +4,6 @@ import torch
 import numpy as np
 import xarray as xr
 
-class SITS_HDF5(Dataset):
-    def __init__(self, h5_path, transforms=None):
-        super().__init__()
-        self.h5_file = h5py.File(h5_path, "r")
-        self.data = self.h5_file["data"]   # shape: (N, T, C, H, W)
-        self.mask = self.h5_file["mask"]   # shape: (N, 1, H, W)
-        self.N = self.data.shape[0]
-        self.transforms = transforms
-
-    def __len__(self):
-        return self.N
-
-    def __getitem__(self, idx):
-        img_np = self.data[idx]   # shape: (T, C, H, W)
-        msk_np = self.mask[idx]   # shape: (1, H, W)
-        sample = {
-            "img": torch.from_numpy(img_np),
-            "msk": torch.from_numpy(msk_np)
-        }
-        if self.transforms:
-            sample = self.transforms(sample)
-        return sample
-
 class SITS(Dataset):
     def __init__(self, data_paths, transforms=None):
         super().__init__()
