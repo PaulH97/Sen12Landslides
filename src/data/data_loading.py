666363dcbd0aeadad245d1776488c9341ade298a
Paul Höhn <paul.hoehn@dlr.de>
2025-03-19 11:34:42 +0100

    Adapt to new transforms

--- src/data/data_loading.py
+++ src/data/data_loading.py
@@ -3,7 +3,7 @@ import json
 import os
 from torch.utils.data import DataLoader
 
-from src.data.transforms import Compose, NoDataReplace, RandomFlip, Normalize, RemoveChannel
+from src.data.transforms import Compose, Normalize, RemoveChannel, RandomFlip
 from src.data.datasets import SITS
 
 def get_dataloaders(cfg):
@@ -19,21 +19,27 @@ def get_dataloaders(cfg):
     dataloaders = {}
     for dataset_type in ["train", "val", "test"]:
         transforms_pipeline = Compose([
-            NoDataReplace(global_means=mean, nodata_value=cfg.dataset.nodata_value),
             RemoveChannel(channel_idx=rm_channel_idx),
             Normalize(mean=mean, std=std, rm_channel_idx=rm_channel_idx),
         ])
 
+        if cfg.model.name == "stvit":
+            transforms_pipeline = Compose([
+            RemoveChannel(channel_idx=rm_channel_idx),
+            Normalize(mean=mean, std=std, rm_channel_idx=rm_channel_idx),
+        ])
+        
         data_paths_file = exp_dir / f"data_paths.json"
         with open(data_paths_file, "r") as f:
             data_paths = json.load(f)
 
         data_paths = [Path(cfg.base_dir) / path for path in data_paths[dataset_type]] # make absolut paths
-        
+        # data_paths = [path for path in data_paths if "chimanimani" in path.name]
+
         data_loader = DataLoader(
             dataset=SITS(data_paths, transforms=transforms_pipeline),
             batch_size=cfg.dataset.batch_size,
-            num_workers=int(os.getenv('SLURM_CPUS_PER_TASK', 8)),
+            num_workers=int(os.getenv('SLURM_CPUS_PER_TASK', 8))-2, # 2 CPUs for other tasks
             shuffle=True if dataset_type == "train" else False,
             pin_memory=True
         )
